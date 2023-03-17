093cee075eb46af671d456be0f9058d9148fc868
jihyunglee <jhlee7125@gmail.com>
2023-03-18 02:33:45 +0900

    feat: main.ts를 app.ts로 변경 + 구현 방법의 차이

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import Page from './components/page';
-
-const initApp = (): void => {
-  const page = Page.instance;
-  page.render();
-};
-
-document.addEventListener('DOMContentLoaded', initApp);
