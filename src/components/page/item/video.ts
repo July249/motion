85bb0aa77783d26e8b785cc98ddbbc04749e5881
jihyunglee <jhlee7125@gmail.com>
2023-03-18 11:59:08 +0900

    refactor: refactor VideoComponent for converting videoId by using RegEx

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -12,11 +12,22 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
     const videoElement = this.element.querySelector(
       '.video__iframe'
     )! as HTMLIFrameElement;
-    videoElement.src = url; // url -> videoId -> embed
+    videoElement.src = this.convertToEmbeddedURL(url); // url -> videoId -> embed
 
     const titleElement = this.element.querySelector(
       '.video__title'
     )! as HTMLHeadingElement;
     titleElement.textContent = title;
+  }
+
+  private convertToEmbeddedURL(url: string): string {
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
   }
 }
