2910f7342aedb873390a4924cd8f98dfa0db89f5
jihyunglee <jhlee7125@gmail.com>
2023-03-18 11:19:32 +0900

    fix: remove label element and replace it to input element by using insertAdjacentText API

--- src/components/page/item/todo.ts
+++ src/components/page/item/todo.ts
@@ -1,11 +1,10 @@
 import { BaseComponent } from '../../component';
 
 export class TodoComponent extends BaseComponent<HTMLElement> {
-  constructor(title: string, todo: string, index: number) {
+  constructor(title: string, todo: string) {
     super(`<section class='todo'>
             <h2 class='todo__title'></h2>
             <input type='checkbox' class='todo-checkbox' />
-            <label></label>
           </section>`);
 
     const titleElement = this.element.querySelector(
@@ -13,15 +12,9 @@ export class TodoComponent extends BaseComponent<HTMLElement> {
     )! as HTMLHeadingElement;
     titleElement.textContent = title;
 
-    const checkboxElement = this.element.querySelector(
+    const todoElement = this.element.querySelector(
       '.todo-checkbox'
     )! as HTMLInputElement;
-    checkboxElement.id = String(index);
-
-    const todoElement = this.element.querySelector(
-      'label'
-    )! as HTMLLabelElement;
-    todoElement.htmlFor = String(index);
-    todoElement.textContent = todo;
+    todoElement.insertAdjacentText('afterend', todo);
   }
 }
