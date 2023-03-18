b45020ab0b5b9c8040ae6cb54b0874f400fd0ccb
jihyunglee <jhlee7125@gmail.com>
2023-03-18 17:33:17 +0900

    refactor: refactor attachTo -> addChild by using Composable interface

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import { Component } from './components/component';
+import { Composable } from './components/page/page';
 import { TodoComponent } from './components/page/item/todo';
 import { NoteComponent } from './components/page/item/note';
 import { ImageComponent } from './components/page/item/image';
@@ -5,7 +7,7 @@ import { PageComponent } from './components/page/page.js';
 import { VideoComponent } from './components/page/item/video';
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
 
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
@@ -15,19 +17,19 @@ class App {
       'Image Title',
       'https://picsum.photos/600/300'
     );
-    image.attachTo(appRoot, 'beforeend');
+    this.page.addChild(image);
 
     const note = new NoteComponent('Note Title', 'Note Body');
-    note.attachTo(appRoot, 'beforeend');
+    this.page.addChild(note);
 
     const todo = new TodoComponent('Todo Title', 'Todo Item');
-    todo.attachTo(appRoot, 'beforeend');
+    this.page.addChild(todo);
 
     const video = new VideoComponent(
       'imase - Night Dancer',
       'https://www.youtube.com/embed/--mDcWPbzFs'
     );
-    video.attachTo(appRoot, 'beforeend');
+    this.page.addChild(video);
   }
 }
 
