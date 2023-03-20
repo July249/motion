74e069aaa504dc16cf2d003599e226df4bfcfb4a
jihyunglee <jhlee7125@gmail.com>
2023-03-21 05:59:04 +0900

    feat: add removeFrom API

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,11 +1,11 @@
 export interface Component {
   attachTo(parent: HTMLElement, position?: InsertPosition): void;
+  removeFrom(parent: HTMLElement): void;
 }
 
 /**
  * Encapsulate the HTML element creation
  */
-
 export class BaseComponent<T extends HTMLElement> {
   protected readonly element: T;
   constructor(htmlString: string) {
@@ -16,5 +16,12 @@ export class BaseComponent<T extends HTMLElement> {
 
   attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
     parent.insertAdjacentElement(position, this.element);
+  }
+
+  removeFrom(parent: HTMLElement) {
+    if (parent !== this.element.parentElement) {
+      throw new Error('Parent mismatch!');
+    }
+    parent.removeChild(this.element);
   }
 }
